90a93142553ad4b9c9b55b7e6743b9fd4e7da685
Vlad Loran <vladloran17@gmail.com>
2026-03-26 20:50:37 +0000

    Finished task

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.exceptions.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MovieDaoImpl extends AbstractDao implements MovieDao {
     public MovieDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,41 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
 
     @Override
     public Movie add(Movie movie) {
-        return null;
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = factory.openSession();
+            tx = session.beginTransaction();
+            session.save(movie);
+            tx.commit();
+            return movie;
+        } catch (RuntimeException e) {
+            if (tx != null) {
+                tx.rollback();
+                throw new DataProcessingException("Cannot add new movie to DB", e);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return null;
+        Session session = null;
+        Movie movie = null;
+        try {
+            session = factory.openSession();
+            movie = session.get(Movie.class, id);
+        } catch (RuntimeException e) {
+            throw new DataProcessingException("Cannot get movie with ID" + id + " ", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return Optional.ofNullable(movie);
     }
 }
